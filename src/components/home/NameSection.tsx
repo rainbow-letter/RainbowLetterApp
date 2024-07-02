2fa027071e134c7920e430ae1e51c557b7189556
Hyuk <poiu03290@naver.com>
2024-07-02 12:57:46 +0900

    feat: chip 컴포넌트 구현

--- src/components/home/NameSection.tsx
+++ src/components/home/NameSection.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import { THEME } from '../../constants/theme';
+
+import Chip from '../common/Chip';
 
 type Props = {
   petsNames: string[];
@@ -12,23 +13,12 @@ const NameSection = ({ petsNames, onClick, selectedPet }: Props) => {
   return (
     <View style={styles.petsNameWrap}>
       {petsNames.map(name => (
-        <Pressable
+        <Chip
           key={`petsName-${name}`}
-          onPress={() => onClick(name)}
-          style={
-            selectedPet !== name
-              ? styles.chip
-              : [styles.chip, styles.selectedChip]
-          }>
-          <Text
-            style={
-              selectedPet !== name
-                ? styles.chipText
-                : [styles.chipText, styles.selectedChipText]
-            }>
-            {name}
-          </Text>
-        </Pressable>
+          value={name}
+          isSelected={selectedPet === name}
+          onClick={() => onClick(name)}
+        />
       ))}
     </View>
   );
@@ -41,24 +31,5 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     gap: 10,
-  },
-  chip: {
-    paddingVertical: 13,
-    paddingHorizontal: 17,
-    borderRadius: 100,
-    borderWidth: 1,
-    borderColor: THEME.COLOR.GRAY_1,
-  },
-  selectedChip: {
-    backgroundColor: THEME.COLOR.ORANGE_3,
-    borderColor: THEME.COLOR.ORANGE_1,
-  },
-  chipText: {
-    color: THEME.COLOR.BLACK_1,
-    fontSize: 14,
-  },
-  selectedChipText: {
-    color: THEME.COLOR.ORANGE_1,
-    fontWeight: 'bold',
   },
 });
