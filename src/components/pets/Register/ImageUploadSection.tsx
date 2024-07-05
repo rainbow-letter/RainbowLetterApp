9cf4b61f220ffe04a5abfbde217912a18b497608
Hyuk <poiu03290@naver.com>
2024-07-05 13:17:51 +0900

    feat: 반려동물 페이지 이미지 업로드 기능 구현

--- src/components/pets/Register/ImageUploadSection.tsx
+++ src/components/pets/Register/ImageUploadSection.tsx
@@ -1,5 +1,15 @@
-import { StyleSheet, Text, View, Pressable, Image, Alert } from 'react-native';
-import React, { useState } from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Image,
+  Alert,
+} from 'react-native';
+import React, { useCallback, useState } from 'react';
+import ImagePicker from 'react-native-image-crop-picker';
+import ImageResizer from 'react-native-image-resizer';
 
 import { StyledPetRegisterTitle } from '../../../model/Pet.model';
 import Plus from '../../../assets/ic_register_plus.svg';
@@ -7,15 +17,56 @@ import Cancel from '../../../assets/ic_register_cancel.png';
 import { THEME } from '../../../constants/theme';
 
 const ImageUploadSection = ({ titleStyle }: StyledPetRegisterTitle) => {
-  const [isShow, setIsShow] = useState(true);
+  const [preview, setPreview] = useState<{ uri: string }>();
+  const [image, setImage] = useState<{
+    uri: string;
+    name: string;
+    type: string;
+  }>();
+
+  const onResponse = useCallback(
+    async (response: any) => {
+      setPreview({ uri: `data:${response.mime};base64,${response.data}` });
+      return ImageResizer.createResizedImage(
+        response.path,
+        600,
+        600,
+        response.mime.includes('jpeg') ? 'JPEG' : 'PNG',
+        100,
+        0,
+      ).then(r => {
+        setImage({
+          uri: r.uri,
+          name: r.name,
+          type: response.mime,
+        });
+        console.log(image);
+      });
+    },
+    [image],
+  );
+
+  const onChangeFile = useCallback(() => {
+    return ImagePicker.openPicker({
+      includeExif: true,
+      includeBase64: true,
+      mediaType: 'photo',
+    })
+      .then(onResponse)
+      .catch(console.log);
+  }, [onResponse]);
 
   return (
     <View style={styles.section}>
       <Text style={titleStyle}>프로필 사진</Text>
-      <Pressable style={styles.upload}>
-        <Plus />
+      <Pressable style={styles.upload} onPress={onChangeFile}>
+        {preview ? (
+          <Image style={styles.previewImage} source={preview} />
+        ) : (
+          <Plus />
+        )}
 
-        {isShow && (
+        {preview && (
           <Pressable
             style={styles.cancelButton}
             hitSlop={8}
@@ -43,6 +94,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: 'dashed',
     position: 'relative',
+  },
+  previewImage: {
+    height: Dimensions.get('window').height / 3,
+    resizeMode: 'contain',
   },
   cancelButton: {
     width: 26,
