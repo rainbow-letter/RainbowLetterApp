74997834fd37f9080ce29ea0df046bb695e9035f
Hyuk <poiu03290@naver.com>
2024-07-29 23:55:11 +0900

    style: NoPets 컴포넌트 수정(스타일 및 이미지 삽입)

--- src/components/common/NoPets.tsx
+++ src/components/common/NoPets.tsx
@@ -1,15 +1,27 @@
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import Button from './Button';
 import { THEME } from '../../constants/theme';
+import House from '../../assets/im_letterBox_house.png';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../../Appinner';
 
 const NoPets = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const onClickPetRegisterButton = useCallback(() => {
+    navigation.navigate('Register');
+  }, [navigation]);
+
   return (
-    <View>
+    <View style={styles.screen}>
+      <Image source={House} />
       <Text style={styles.title}>앗, 편지를 받을 아이가 없어요</Text>
       <Text style={styles.contents}>우리 아이를 등록하러 가볼까요?</Text>
-      <Button onPress={() => Alert.alert('구현중')} isCheck>
+      <Button onPress={onClickPetRegisterButton} isCheck>
         등록하기
       </Button>
     </View>
@@ -19,12 +31,21 @@ const NoPets = () => {
 export default NoPets;
 
 const styles = StyleSheet.create({
+  screen: {
+    height: '100%',
+    backgroundColor: THEME.COLOR.WHITE,
+    paddingHorizontal: 18,
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
     color: THEME.COLOR.BLACK_1,
     textAlign: 'center',
     marginBottom: 12,
+    marginTop: 20,
   },
   contents: {
     textAlign: 'center',
